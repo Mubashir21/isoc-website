0e0dcaba585f6492fe319773b072e37f65583bd6
Mubashir21 <mubashirshoukat@gmail.com>
2024-07-31 15:35:31 +0300

    pseudo code for route - temp

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -41,3 +41,9 @@
 //     );
 //   }
 // }
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ message: "File uploaded successfully" });
+}
