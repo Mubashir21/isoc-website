52681203b63c34f7bb7309e9d536265d3d8dc238
Mubashir21 <mubashirshoukat@gmail.com>
2025-03-01 19:38:31 +0800

    started tracking analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
   subsets: ["latin"],
